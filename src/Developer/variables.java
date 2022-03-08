704b750abe551f22d3b26849b2f5f2e2d3a9196c
haydee27 <otokeincognita@gamil.com>
2022-03-08 15:06:44 -0600

    Miprimerproyecto

---
+++ src/Developer/variables.java
@@ -0,0 +1,19 @@
+
+package Developer;
+
+
+public class variables {
+
+   
+    public static void main(String[] args) {
+        
+        int suma, a, b;
+        a =12;
+        b= 30;
+        
+        suma= a + b;
+        
+        System.out.println("La suma es: " + suma );
+    }
+    
+}
